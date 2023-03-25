4f6c6de4c89d70678ba2579ade81c7ad2390b9c2
Abdulaziz <learntodevelop2020@gmail.com>
2023-03-25 14:45:21 +0500

    Adding more tests to Service layer

--- repository/src/main/java/com/epam/esm/model/Tag.java
+++ repository/src/main/java/com/epam/esm/model/Tag.java
@@ -3,10 +3,12 @@ package com.epam.esm.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Tag implements Comparable<Tag> {
     private Long id;
     private String name;
